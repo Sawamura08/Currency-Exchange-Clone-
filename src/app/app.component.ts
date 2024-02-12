6c264f9c237e021815432149d1b77a1d7fd56af9
LinTech <hernandezmarkjulius24@gmail.com>
2024-02-12 08:38:05 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from './currency.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'currencyExhange';
+export class AppComponent implements OnInit {
+  currencies: any;
+
+  constructor(private currencyService: CurrencyService) {}
+
+  ngOnInit() {
+    this.currencyService.getCurrencies().subscribe((data) => {
+      this.currencies = data;
+      console.log(this.currencies);
+    });
+  }
 }
